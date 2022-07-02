2230b0bd33f196c82c2dba67f31700792b760755
AustinT-3 <108505523+AustinT-3@users.noreply.github.com>
2022-07-01 20:19:36 -0500

    Add a loop and newlines

--- coinflip.py
+++ coinflip.py
@@ -9,31 +9,35 @@ def main():
         if input1 in [ "Y", 'y']:
             coinface = random.randint(0, 1)
 
-            input2 = input("Heads or Tails? ")
+            input2 = input("\nHeads or Tails? ")
 
             if input2 in [ "heads", "Heads" ]:
                 if coinface == 0:
-                    print("You Win! The coin was heads!")
-                    print(coinface)
+                    print("\nYou Win! The coin was heads!")
                 else:
-                    print("You Lose! The coin was tails!")
-                    print(coinface)
+                    print("\nYou Lose! The coin was tails!")
             elif input2 in [ "tails", "Tails" ]:
                 if coinface == 0:
-                    print("You Lose! The coin was heads!")
-                    print(coinface)
+                    print("\nYou Lose! The coin was heads!")
                 else:
-                    print("You Win! The coin was tails!")
-                    print(coinface)
+                    print("\nYou Win! The coin was tails!")
             else:
                 print("Invalid Input! Input Y or N.")
                 sys.exit(0)
+            
+            quitgame = input("Play Again? [Y/N]:  ")
+            if quitgame in [ "Y", 'y']:
+                print("\n")
+                main()
+            else:
+                print("Quitting...")
+                sys.exit(0)
 
         elif input1 in [ "N", 'n']:
-            print("Quitting...")
+            print("\nQuitting...")
             sys.exit(0)
         else:
-            print("Invalid Input! Input Y or N.")
+            print("\nInvalid Input! Input Y or N.")
             sys.exit(0)
 
 main()
